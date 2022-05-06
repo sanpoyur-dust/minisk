6e70d829205caa66c7c1452e58528ed279804abb
Jack Xu <xxu447@wisc.edu>
2022-05-06 01:31:24 -0500

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    java
+}
+
+group = "org.semgus"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://jitpack.io") { name = "Jitpack" }
+}
+
+dependencies {
+    implementation("com.googlecode.json-simple:json-simple:1.1.1")
+    implementation("com.github.SemGuS-git:Semgus-Java:1.0.1")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
